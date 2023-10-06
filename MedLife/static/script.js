0b4a314f232958cc4502fcc12799f03e9db30b1a
rahimul12 <117390063+rahimul12@users.noreply.github.com>
2023-10-07 00:23:35 +0600

    Add files via upload

---
+++ MedLife/static/script.js
@@ -0,0 +1,20 @@
+
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true,
+//     smartphone: {
+//         smooth: true
+//     }
+// });
+
+// const linkContainer = document.getElementById("column2");
+// const links = linkContainer.getElementsByClassName("nav-link");
+
+// for(var i = 0; i < links.length; i++){
+//     links[i].addEventListener('click', function(){
+//         var current = document.getElementsByClassName("active");
+//         current[0].className = current[0].className.replace(" active");
+//         this.className += " active";
+//     })
+// }
+
